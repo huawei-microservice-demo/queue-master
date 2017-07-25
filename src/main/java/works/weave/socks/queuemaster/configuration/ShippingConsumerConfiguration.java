ac82cf3c65598246ac98d89aff8204dbeaa6649f
preethikathamuthu <preethi.kathamuthu@huawei.com>
2017-07-25 16:21:49 +0000

    queuemaster updated with perf-stats

--- src/main/java/works/weave/socks/queuemaster/configuration/ShippingConsumerConfiguration.java
+++ src/main/java/works/weave/socks/queuemaster/configuration/ShippingConsumerConfiguration.java
@@ -10,35 +10,34 @@ import org.springframework.context.annotation.Configuration;
 import works.weave.socks.queuemaster.ShippingTaskHandler;
 
 @Configuration
-public class ShippingConsumerConfiguration extends RabbitMqConfiguration
-{
-	protected final String queueName = "shipping-task";
+public class ShippingConsumerConfiguration extends RabbitMqConfiguration {
+    protected final String queueName = "shipping-task";
 
     @Autowired
     private ShippingTaskHandler shippingTaskHandler;
 
-	@Bean
-	public RabbitTemplate rabbitTemplate() {
-		RabbitTemplate template = new RabbitTemplate(connectionFactory());
-		template.setQueue(this.queueName);
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory());
+        template.setQueue(this.queueName);
         template.setMessageConverter(jsonMessageConverter());
-		return template;
-	}
+        return template;
+    }
 
     @Bean
-	public Queue queueName() {
-		return new Queue(this.queueName, false);
-	}
+    public Queue queueName() {
+        return new Queue(this.queueName, false);
+    }
 
-	@Bean
-	public SimpleMessageListenerContainer listenerContainer() {
-		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-		container.setConnectionFactory(connectionFactory());
-		container.setQueueNames(this.queueName);
-		container.setMessageListener(messageListenerAdapter());
+    @Bean
+    public SimpleMessageListenerContainer listenerContainer() {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory());
+        container.setQueueNames(this.queueName);
+        container.setMessageListener(messageListenerAdapter());
 
-		return container;
-	}
+        return container;
+    }
 
     @Bean
     public MessageListenerAdapter messageListenerAdapter() {
